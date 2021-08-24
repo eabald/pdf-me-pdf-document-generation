2bc778a14ce8718bd77eb7e2f7da1bdb02778416
Maciej Krawczyk <maciejro.krawczyk@gmail.com>
2021-08-24 21:43:16 +0200

    limits usage wip

--- src/document-generation/document-generation.service.ts
+++ src/document-generation/document-generation.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, HttpStatus } from '@nestjs/common';
 import { GenerateDocumentDto } from '@pdf-me/shared';
 import { InjectContext } from 'nest-puppeteer';
 import { BrowserContext, PDFOptions } from 'puppeteer';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import Handlebars from 'handlebars';
 
 @Injectable()
@@ -11,8 +11,19 @@ export class DocumentGenerationService {
     @InjectContext() private readonly browserContext: BrowserContext,
     @Inject('TEMPLATES_SERVICE') private templatesService: ClientProxy,
     @Inject('FILES_SERVICE') private filesService: ClientProxy,
+    @Inject('LIMITS_SERVICE') private limitsService: ClientProxy,
   ) {}
   async generate(data: GenerateDocumentDto) {
+    // check limit
+    const limit = await this.limitsService
+      .send({ cmd: 'limits-check' }, data.userId)
+      .toPromise();
+    if (limit.currentLimit < 1) {
+      throw new RpcException({
+        message: 'Wrong credentials provided',
+        statusCode: HttpStatus.PAYMENT_REQUIRED,
+      });
+    }
     // compile template
     const template = await this.templatesService
       .send({ cmd: 'templates-get-by-id' }, data.templateId)
